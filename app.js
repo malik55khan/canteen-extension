0b3022c3d2d981fdf311d19d381554f5ff6e16f7
Malik Khan <malikkhan@smartdatainc.net>
2019-12-25 23:59:19 +0530

    code updted

--- app.js
+++ app.js
@@ -30,43 +30,6 @@ require('./modules/order/order.route')(app,express,passport);
 require('./modules/payment/payment.route')(app,express,passport);
 require('./modules/setting/setting.route')(app,express,passport);
 require('./modules/notification/notification.route')(app,express,passport);
-
-
-// require('./modules/admin/admin.route')(app,express,passport);
-// require('./modules/cms-page/cms.route')(app,express,passport);
-// require('./modules/fileuploader/fileuploader.route')(app,express,passport);
-// require('./modules/speciality/speciality.route')(app,express,passport);
-// require('./modules/questionnaire/questionnaire.route')(app,express,passport);
-// require('./modules/orderkit/orderkit.route')(app,express,passport);
-// require('./modules/order/order.route')(app,express,passport);
-// require('./modules/stripe/stripe.route')(app,express,passport);
-// require('./modules/search/search.route')(app,express,passport);
-// require('./modules/appointment/appointment.route')(app,express,passport);
-// require('./modules/shared/shared.route')(app,express,passport);
-
-app.use('/assets', express.static(path.join(__dirname, '/dist/adminDist/dist/assets/')));
-app.use('/admin', express.static(path.join(__dirname, '/dist/adminDist/dist')));
-app.get('/admin', function (req, res) {
-  return res.sendFile(path.join(__dirname, '/dist/adminDist/dist/index.html'))
-}).get('/admin/*', function (req, res) {
-  return res.sendFile(path.join(__dirname, '/dist/adminDist/dist/index.html'))
-});
-
-app.use('/assets', express.static(path.join(__dirname, '/dist/specialistDist/dist/assets/')));
-app.use('/specialist', express.static(path.join(__dirname, '/dist/specialistDist/dist')));
-app.get('/specialist', function (req, res) {
-  return res.sendFile(path.join(__dirname, '/dist/specialistDist/dist/index.html'))
-}).get('/specialist/*', function (req, res) {
-  return res.sendFile(path.join(__dirname, '/dist/specialistDist/dist/index.html'))
-})
-
-app.use('/assets', express.static(path.join(__dirname, '/dist/checkisaurus/assets/')));
-app.use('/', express.static(path.join(__dirname, '/dist/checkisaurus/')));
-app.get('/', function (req, res) {
-  return res.sendFile(path.join(__dirname, '/dist/checkisaurus/index.html'))
-}).get('/*', function (req, res) {
-  return res.sendFile(path.join(__dirname, '/dist/checkisaurus/index.html'))
-})
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
